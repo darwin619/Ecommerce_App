9ff79887334412c1afc65807c61e01bfc412ad3a
darwin619 <shivamaima619@gmail.com>
2018-01-13 12:00:00 +0100

    Added styling to menu items

--- src/Components/MenuItem/MenuItem.js
+++ src/Components/MenuItem/MenuItem.js
@@ -3,10 +3,15 @@ import './MenuItem.scss';
 
 const MenuItem = ( {title, imageUrl, large} ) => {
 	return (
-		<div 
-		style={{backgroundImage: `url(${imageUrl})`
+		<div className={`${large} menu-item`}>
 		
-	}}  className={`${large} menu-item`} >
+			<div 
+			className="background-image"
+			style={{
+				backgroundImage: `url(${imageUrl})` 
+			}} 
+			/>
+
 
 					<div className="content">
 						<h1 className='title'>{title}</h1>
