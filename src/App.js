5b6df1149b980a06c795a00480d29ec249c5aea5
darwin619 <shivamaima619@gmail.com>
2019-07-18 17:17:25 +0530

    Added project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Homepage from './Pages/Homepage/Homepage'
+
+function App() {
+  return (
+    <div className = "Homepage"> 
+         <Homepage /> 
+    </div>
+  );
+}
+
+export default App;
