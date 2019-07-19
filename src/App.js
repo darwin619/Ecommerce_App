b9374ef09c8259af7ba9896c77c576d555c04108
darwin619 <shivamaima619@gmail.com>
2019-07-19 16:17:35 +0530

    Added Navbar with react router link

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header/Header';
 import Homepage from './Pages/Homepage/Homepage';
 import { Switch, Route } from 'react-router-dom';
 import Shop from './Pages/Shop/Shop';
 
 
+
 function App() {
   return (
     <div className = "Homepage">
+    <Header />
     <Switch> 
          <Route exact path='/' component={Homepage} /> 
          <Route exact path='/shop' component={Shop} /> 
